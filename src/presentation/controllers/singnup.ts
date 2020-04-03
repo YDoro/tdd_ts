2c7af54565e1e0df635beb4e75ad0ec4c45155cb
Yan <yan_mvd@hotmail.com>
2020-04-03 19:26:11 -0300

    feat: ensure SignUpController returns 422 if no email is provided

--- src/presentation/controllers/singnup.ts
+++ src/presentation/controllers/singnup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 422,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 422,
+        body: new Error('Missing param: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 422,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
