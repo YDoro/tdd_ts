7004d448e00f67f6846d30c481b318b7babbda46
Yan <yan_mvd@hotmail.com>
2020-04-03 19:26:25 -0300

    test: ensure SignUpController returns 422 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(422)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 422 if no email is provided', () => {
+    // sut = system under test, the class that will be tested
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_pass',
+        passwordConfirm: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(422)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
