86064e9abe4846e0d5e3e8592da0b351c53ab045
Yan <yan_mvd@hotmail.com>
2020-04-07 11:44:46 -0300

    feat: ensure signUpController returns 422 if an invalid email is provided

--- src/presentation/controllers/singnup.ts
+++ src/presentation/controllers/singnup.ts
@@ -2,11 +2,21 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { unprocessableEntity } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emaiilValidator: EmailValidator) {
+    this.emailValidator = emaiilValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) return unprocessableEntity(new MissingParamError(field))
+    }
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return unprocessableEntity(new InvalidParamError('email'))
     }
   }
 }
