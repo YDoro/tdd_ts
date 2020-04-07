c008df0751c70f465d8ba350fa8ed146dd0ac845
Yan <yan_mvd@hotmail.com>
2020-04-07 18:21:31 -0300

    feat: ensure SignUpController returns 500 if EmailValidator Throws

--- src/presentation/controllers/singnup.ts
+++ src/presentation/controllers/singnup.ts
@@ -4,6 +4,7 @@ import { unprocessableEntity } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   constructor (emaiilValidator: EmailValidator) {
@@ -11,12 +12,16 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return unprocessableEntity(new MissingParamError(field))
-    }
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return unprocessableEntity(new InvalidParamError('email'))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return unprocessableEntity(new MissingParamError(field))
+      }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return unprocessableEntity(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return { statusCode: 500, body: new ServerError() }
     }
   }
 }
