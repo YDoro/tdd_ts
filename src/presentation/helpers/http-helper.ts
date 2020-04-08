bdce36bf9ebfb511e0396c78da43f2304f951ce5
Yan <yan_mvd@hotmail.com>
2020-04-08 11:29:38 -0300

    feat: created OK and Created helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,14 @@
 import { HttpResponse } from '../protocols/http'
 import { ServerError } from '../errors/server-error'
 
+export const OK = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+export const Created = (data: any): HttpResponse => ({
+  statusCode: 201,
+  body: data
+})
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
