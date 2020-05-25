e07f519cff89f0281580f70ae77c795661547b3c
Yan <yan_mvd@hotmail.com>
2020-05-24 22:39:55 -0300

    feat: ensure login controller returns 422 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,6 +4,11 @@ import { unprocessableEntity } from '../../helpers/http-helper'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('email'))))
+    if (!httpRequest.body.email) {
+      return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('email'))))
+    }
+    if (!httpRequest.body.password) {
+      return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('password'))))
+    }
   }
 }
