d8980b8cd267bea9020b38cb8fd68b2b04aaf7cd
Yan <yan_mvd@hotmail.com>
2020-06-01 16:54:19 -0300

    feat: ensure LoginController returns 500 if email validator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpResponse, HttpRequest } from '../../protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { unprocessableEntity } from '../../helpers/http-helper'
+import { unprocessableEntity, serverError } from '../../helpers/http-helper'
 import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
@@ -10,16 +10,20 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('email'))))
-    }
-    if (!password) {
-      return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('password'))))
-    }
-    const isVaild = this.emailValidator.isValid(email)
-    if (!isVaild) {
-      return await new Promise(resolve => resolve(unprocessableEntity(new InvalidParamError('email'))))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('email'))))
+      }
+      if (!password) {
+        return await new Promise(resolve => resolve(unprocessableEntity(new MissingParamError('password'))))
+      }
+      const isVaild = this.emailValidator.isValid(email)
+      if (!isVaild) {
+        return await new Promise(resolve => resolve(unprocessableEntity(new InvalidParamError('email'))))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
