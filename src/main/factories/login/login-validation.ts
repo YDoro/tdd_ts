57eb20f178316ac8a3ce1a5508de635bf50a7ed9
Yan <yan_mvd@hotmail.com>
2020-06-11 01:35:09 -0300

    refactor: created validators index

--- src/main/factories/login/login-validation.ts
+++ src/main/factories/login/login-validation.ts
@@ -1,8 +1,7 @@
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation, ValidationComposite, EmailValidation } from '../../../presentation/helpers/validators/'
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['email', 'password']) {
