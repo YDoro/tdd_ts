52e5798cecec24892cf21b5621daeea8f4c655cc
Yan <yan_mvd@hotmail.com>
2020-06-27 17:17:05 -0300

    test: ensure post /login returns 200

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,19 +1,22 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+let accountCollection: Collection
 describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   describe('POST /signup', () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-    beforeEach(async () => {
-      const accountCollection = await MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
-    test('Should return status 200 on signup', async () => {
+    test('Should return status 201 on signup', async () => {
       await request(app).post('/api/signup')
         .send({
           name: 'yan',
@@ -22,6 +25,23 @@ describe('Login Routes', () => {
           email: 'some_email@mail.com'
         })
         .expect(201)
+    })
+  })
+  describe('POST /login', () => {
+    test('Should return status 200 on login', async () => {
+      const password = await hash('some_password', 12)
+      await accountCollection.insertOne({
+        name: 'yan',
+        password,
+        passwordConfirm: 'some_password',
+        email: 'some_email@mail.com'
+      })
+      await request(app).post('/api/login')
+        .send({
+          email: 'some_email@mail.com',
+          password: 'some_password'
+        })
+        .expect(200)
     })
   })
 })
