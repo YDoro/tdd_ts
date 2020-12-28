e91005693f4d811143c57fda6f9359ca5072c2bc
Yan <yan_mvd@hotmail.com>
2020-12-28 13:07:19 -0300

    refactor: i dont know

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -4,6 +4,7 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -14,6 +15,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Should return status 403 on add survey without accessToken', async () => {
@@ -29,5 +32,34 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
+    // test('should return 204 on add survey with valid token', async () => {
+    //   const res = await accountCollection.insertOne({
+    //     name: 'yan',
+    //     password: '123',
+    //     email: 'some_email@mail.com',
+    //     role: 'admin'
+    //   })
+    //   const id = res.ops[0]._id
+    //   const accessToken = sign({ id }, env.jwtSecret)
+    //   await accountCollection.updateOne({
+    //     _id: id
+    //   }, {
+    //     $set: {
+    //       accessToken
+    //     }
+    //   })
+    //   await request(app).post('/api/surveys')
+    //     .send({
+    //       question: 'Question',
+    //       answers: [{
+    //         answer: 'Answer 1',
+    //         image: 'http://imagename.domain.com'
+    //       }, {
+    //         answer: 'Answer 2'
+    //       }]
+    //     })
+    //     .set('x-access-token', accessToken)
+    //     .expect(204)
+    // })
   })
 })
